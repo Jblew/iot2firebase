ebb129ad4ca10aca6851b69392a59f5e66f96b6d
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2020-06-14 19:17:04 +0200

    feat: configure i2c pins

--- firmware/src/hardware/i2c.cpp
+++ firmware/src/hardware/i2c.cpp
@@ -2,18 +2,20 @@
 #include "I2CScan.h"
 
 HardwareI2C::HardwareI2C()
-  :wireLeft(0)
-  ,wireRight(1)
-{}
+    : wireBusA(0)
+// ,wireBusB(1)
+{
+}
 
-void HardwareI2C::startI2C() {
-    this->wireLeft.begin(I2C_LEFT_SDA, I2C_LEFT_SCL);
-    this->wireLeft.setClock(I2C_FREQUENCY);
-    I2CScan::scanI2CDevices(this->wireLeft, "left");
-    delay(100);
+void HardwareI2C::startI2C()
+{
+  this->wireBusA.begin(I2C_BUSA_SDA, I2C_BUSA_SCL);
+  this->wireBusA.setClock(I2C_FREQUENCY);
+  I2CScan::scanI2CDevices(this->wireBusA, "busA");
+  delay(100);
 
-    this->wireRight.begin(I2C_RIGHT_SDA, I2C_RIGHT_SCL);
-    this->wireRight.setClock(I2C_FREQUENCY);
-    I2CScan::scanI2CDevices(this->wireRight, "right");
-    delay(100);
+  // this->wireBusB.begin(I2C_BUSB_SDA, I2C_BUSB_SCL);
+  // this->wireBusB.setClock(I2C_FREQUENCY);
+  // I2CScan::scanI2CDevices(this->wireBusB, "busB");
+  // delay(100);
 }
--- firmware/src/hardware/i2c.h
+++ firmware/src/hardware/i2c.h
@@ -3,20 +3,20 @@
 #ifndef _HARDWARE_I2C_H_
 #define _HARDWARE_I2C_H_
 
-
-#define I2C_LEFT_SDA 21
-#define I2C_LEFT_SCL 22
-#define I2C_RIGHT_SDA 19
-#define I2C_RIGHT_SCL 18
+#define I2C_BUSA_SDA 21
+#define I2C_BUSA_SCL 22
+// #define I2C_BUSB_SDA 19
+// #define I2C_BUSB_SCL 18
 #define I2C_FREQUENCY 400000
 
-class HardwareI2C {
-  public:
-    HardwareI2C();
-    void startI2C();
+class HardwareI2C
+{
+public:
+  HardwareI2C();
+  void startI2C();
 
-    TwoWire wireLeft;
-    TwoWire wireRight;
+  TwoWire wireBusA;
+  //TwoWire wireBusB;
 };
 
 #endif
