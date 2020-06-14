088cce5423200f997a9f5ebc96e0c78dc735f2b8
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2020-06-14 17:42:21 +0200

    feat(functions): base go realtime db accessor

--- functions/main.go
+++ functions/main.go
@@ -1,13 +1,26 @@
-package main
+package functions
 
 import (
-	"iot2firebasefunctions/app"
+	"context"
 	"log"
+
+	firebase "firebase.google.com/go"
+	"firebase.google.com/go/db"
 )
 
-func main() {
-	err := app.Run()
+var client *db.Client
+
+func init() {
+	ctx := context.Background()
+	conf := &firebase.Config{
+		DatabaseURL: "https://<CHANGE_ME>.firebaseio.com/",
+	}
+	app, err := firebase.NewApp(ctx, conf)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("firebase.NewApp: %v", err)
+	}
+	client, err = app.Database(ctx)
+	if err != nil {
+		log.Fatalf("app.Firestore: %v", err)
 	}
 }
