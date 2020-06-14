3ea3244a2c69296a0eeace1b5bcc8f5f90ad8ca6
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2020-06-14 17:15:32 +0200

    feat: describe script

---
+++ iot/describe.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "${DIR}"
+
+source ./iot.config.sh
+
+if [ -z "${DEVICE_NAME}" ]; then
+    echo "DEVICE_NAME env is not set"
+    exit 1
+fi
+
+gcloud iot devices describe $DEVICE_NAME \
+  --project=$GCP_PROJECT_ID \
+  --region=$GCP_PROJECT_REGION \
+  --registry=$REGISTRY_NAME
--- project.config.sh
+++ project.config.sh
@@ -4,4 +4,5 @@ PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export NTP_SERVER=time.google.com
 export DEVICES_DIR="${PROJECT_DIR}/iot/devices/"
 export DEVICE_CONFIG_FILENAME="device.config.sh"
-export IOT_CONFIG_FILE="${PROJECT_DIR}/iot/iot.config.sh"
+export GCP_PROJECT_ID="hi-pine"
+export GCP_PROJECT_REGION="europe-west1"
