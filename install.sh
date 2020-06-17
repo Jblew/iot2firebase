ae4844734c847055309d1d478f61c35fc5c3546d
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2020-06-17 19:42:40 +0200

    ci: deploy all script

--- install.sh
+++ install.sh
@@ -7,7 +7,20 @@ echo "# Setting up IoT"
 ./iot/setup.sh
 echo "# IoT setup done"
 
-echo "# Deploying"
+echo "# Generating config files for firmware"
+./firmware/generate-config.sh
+echo "# Generating config files for firmware done"
+
+echo "# Deploying functions"
 ./functions/deploy.sh
 echo "# Functions deploy done"
+
+echo "# Deploying panel"
+./panel/deploy.sh
+echo "# Functions panel done"
+
+echo "# Firestore"
+echo "!!! Please update firestore rules manually (use firestore.rules file)"
+echo ""
+
 echo "# Iot2firebase installation done"
